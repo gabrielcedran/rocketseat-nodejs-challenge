42fd0af1c6c0a5223f54b20629dd82bf89764bea
Gabriel Cedran <gabrielcedran@gmail.com>
2020-04-13 23:26:27 +0100

    Simplest solution to the challenge

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,27 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: RequestDTO): Transaction {
+    if (!title || title.length <= 0) {
+      throw Error('Title is mandatory');
+    }
+
+    if (!value || value <= 0) {
+      throw Error('Value is mandatory');
+    }
+
+    if (!type || (type !== 'income' && type !== 'outcome')) {
+      throw Error('Type is invalid');
+    }
+
+    if (
+      type === 'outcome' &&
+      this.transactionsRepository.getBalance().total < value
+    ) {
+      throw Error('No balance available for this transaction');
+    }
+
+    return this.transactionsRepository.create({ title, value, type });
   }
 }
 
